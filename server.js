16c20f5bb496b0016f4c7b76c97eed7a1f689783
Brian Bennett <bahamat@digitalelf.net>
2017-11-24 09:56:50 -0700

    Working

---
+++ server.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node --abort-on-uncaught-exception
+
+var restify = require('restify'),
+    qr = require('qr-image');
+
+function respond(req, res, next) {
+    res.contentType = 'image/png';
+    res.code = 200;
+    var qr_png = qr.imageSync(req.params.message, { type: 'png' });
+    res.header('Content-Length', qr_png.count);
+    res.header('Content-Disposition', 'attachment; filename=qr.png');
+    res.send(qr_png);
+    console.log('%s %s', res.code, req.url);
+    return (next);
+}
+
+var server = restify.createServer();
+
+server.get({path: '/qr/:message', version: '0.0.1'}, respond);
+server.head({path: '/qr/:message', version: '0.0.1'}, respond);
+
+server.get(/\/$/, restify.serveStatic({
+    directory: './html',
+    file: 'qr.html'
+}));
+
+server.listen(8080, '::', function serverListenCallback() {
+    console.log('%s listening at %s', server.name, server.url);
+});
